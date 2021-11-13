1a4b51301722b14cba115a910729729853205859
gr1mw1k <ameyashinde3@gmail.com>
2021-11-13 22:30:11 +0530

    Homework

--- Fifth_lec_12thNov/Reverse_Pyramid_Pattern.java
+++ Fifth_lec_12thNov/Reverse_Pyramid_Pattern.java
@@ -2,23 +2,21 @@ package Fifth_lec_12thNov;
 
 public class Reverse_Pyramid_Pattern {
 	
-	public static void main(String args[]) {
+public static void main(String args[]) {
 		
 		int n=5;
-		for(int i=n;i>=1;i--) {
+		for(int i=5;i>=0;i--) {
 			
 			for(int j=1;j<=(n-i);j++) {
 				System.out.print(" ");
 			}
 			
 			
-			for(int j=1;j<=i-1;j++){
+			for(int j=1;j<=i;j++){
 				System.out.print("*"+" ");
 			}
 			System.out.println();
 		}
-		
-	 
 	}
 
 }
