5823f97c2b7468961bbc862f2b7a4b518122d7e4
gr1mw1k <ameyashinde3@gmail.com>
2021-12-04 20:22:56 +0530

    Homework

---
+++ _Nov30th/Person.java
@@ -0,0 +1,71 @@
+package _Nov30th;
+
+//2. Modify the code below to add more constructors. Also modify the main method to test the new constructors.
+
+public class Person
+{
+   // fields
+   private String name;
+   private String email;
+   private String phoneNumber;
+   
+   
+
+   public Person()
+   {
+	  System.out.println("Please add field details");
+   }
+
+   
+   public Person(String theName)
+   {
+	  this(theName+"@edu.in","406 558-7725");
+      this.name = theName;
+      
+   }
+   
+  
+   public Person(String theEmail,String thePhoneNumber)
+   {
+      this.email = theEmail;
+      this.phoneNumber=thePhoneNumber;
+   }
+   
+   public Person(String theName,String theEmail,String thePhoneNumber)
+   {
+	  this.name = theName;
+      this.email = theEmail;
+      this.phoneNumber=thePhoneNumber;
+   }
+   
+
+   // methods - getters
+   public String getName() { return this.name;}
+   public String getEmail() { return this.email;}
+   public String getPhoneNumber() { return this.phoneNumber;}
+
+   // methods - setters
+   public void setName(String theName) { this.name = theName;}
+   public void setEmail(String theEmail) {this.email = theEmail;}
+   public void setPhoneNumber(String thePhoneNumber) { this.phoneNumber = thePhoneNumber;}
+   public String toString()
+   {
+      return this.name + " " + this.email + " " + this.phoneNumber;
+   }
+
+   // main method for testing
+   public static void main(String[] args)
+   {
+      Person p1 = new Person("Sana");
+      System.out.println(p1);
+      
+      Person p2 = new Person("Jean");
+      p2.setEmail("jean@gmail.com");
+      p2.setPhoneNumber("404 899-9955");
+      System.out.println(p2);
+      
+      Person p3=new Person("Ameya","ameyashinde3@gmail.com","8329088406");
+      System.out.println(p3);
+   }
+}
+
